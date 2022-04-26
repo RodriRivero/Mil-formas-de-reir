382e61fab24b158bb480e4299e36f9950ebbb37d
rodrigoss88 <guidorodrigorivero@outlook.com>
2022-04-26 00:56:44 -0300

    js rivero

--- js/intro.js
+++ js/intro.js
@@ -1,17 +1,30 @@
 
+const iconoMenu = document.querySelector('#icono-menu')
+menu = document.querySelector('#menu');
 
+iconoMenu.addEventListener('click', (e) => {
+    menu.classList.toggle('active');
+    document.body.classList.toggle('opacity');
 
-function joke(){
-    window.location = "jokes.html"
+    const rutaActual = e.target.getAttribute('src');
+    if(rutaActual == 'img/opem-menu.png'){
+        e.target.setAttribute('src','img/open-menu2.png');
+        }else{
+            e.target.setAttribute('src','img/open-menu2.png');
 }
+});
 
 
- function saludo(){
 
-    let name = document.getElementById("usuario").value;
-    document.getElementById("usuario").innerHTML = 'que quieres hacer el dia de hoy '+ name + '?';
+//function joke(){
+    //window.location = "jokes.html"}
 
-}
+
+// function saludo(){
+
+   // let name = document.getElementById("usuario").value;
+   // document.getElementById("usuario").innerHTML = 'que quieres hacer el dia de hoy '+ name + '?';}
+
 
 
 //function saludo(){
