22187567cfba0736deeb36b6430857ccdea9b7ba
rodrigoss88 <guidorodrigorivero@outlook.com>
2022-04-27 20:49:51 -0300

    2da entrega

--- js/dom3.js
+++ js/dom3.js
@@ -2,5 +2,5 @@
 
  let nameIn = document.getElementById("nameIn")
 
- document.getElementById("nameIn").innerHTML = "MilFormasDeReir"
+ document.getElementById("nameIn").innerHTML = " Bienvenido a      MilFormasDeReir!!"
  
