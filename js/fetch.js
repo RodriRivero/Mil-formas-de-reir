a0f26727aeb721a6b8d8c7c9368f12c08712aa66
rodrigoss88 <guidorodrigorivero@outlook.com>
2022-05-11 23:39:27 -0300

    jsriverofetch

--- js/fetch.js
+++ js/fetch.js
@@ -1,21 +1,26 @@
 
 
 function cargarjokes(){
-    fetch('/audio.json/black.json')
-    .then(respuesta => respuesta.json())
-    .then(respuesta => console.log(respuesta));
 
-    fetch('/audio.json/green.json')
+    fetch('/audio.json/black.json')        //lista 
     .then(respuesta => respuesta.json())
-    .then(respuesta => console.log(respuesta));
+    .then(black =>{
+        black.forEach(black => {
+            console.log(black);
+        });
+    })
+
+    fetch('/audio.json/green.json')        //grupo
+    .then(respuesta => respuesta.json())
+    .then(respuesta => console.log(respuesta))
 
     fetch('/audio.json/motiv.json')
     .then(respuesta => respuesta.json())
-    .then(respuesta => console.log(respuesta));
+    .then(respuesta => console.log(respuesta))
 
     fetch('/audio.json/reflex.json')
     .then(respuesta => respuesta.json())
-    .then(respuesta => console.log(respuesta));
+    .then(respuesta => console.log(respuesta))
 
     fetch('/audio.json/white.json')
     .then(respuesta => respuesta.json())
