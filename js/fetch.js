608bd51b07af2597dcccf1938235747418d2fde7
rodrigoss88 <guidorodrigorivero@outlook.com>
2022-05-12 00:02:42 -0300

    jsfetch

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,7 @@
 
 
 function cargarjokes(){
+    const URL_BASE ='https://rodrigoss88.github.io/audio.json/black.json';
 
     fetch('/audio.json/black.json')        //lista 
     .then(respuesta => respuesta.json())
@@ -10,20 +11,20 @@ function cargarjokes(){
         });
     })
 
-    fetch('/audio.json/green.json')        //grupo
-    .then(respuesta => respuesta.json())
-    .then(respuesta => console.log(respuesta))
+   /// fetch('/audio.json/green.json')        //grupo
+    //.then(respuesta => respuesta.json())
+   // .then(respuesta => console.log(respuesta))
 
-    fetch('/audio.json/motiv.json')
-    .then(respuesta => respuesta.json())
-    .then(respuesta => console.log(respuesta))
+    //fetch('/audio.json/motiv.json')
+    //.then(respuesta => respuesta.json())
+    //.then(respuesta => console.log(respuesta))
 
-    fetch('/audio.json/reflex.json')
-    .then(respuesta => respuesta.json())
-    .then(respuesta => console.log(respuesta))
+   // fetch('/audio.json/reflex.json')
+    //.then(respuesta => respuesta.json())
+    //.then(respuesta => console.log(respuesta))
 
-    fetch('/audio.json/white.json')
-    .then(respuesta => respuesta.json())
-    .then(respuesta => console.log(respuesta))
+   // fetch('/audio.json/white.json')
+   // .then(respuesta => respuesta.json())
+    //.then(respuesta => console.log(respuesta))
 }
 cargarjokes();
