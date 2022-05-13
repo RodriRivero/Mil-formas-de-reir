ad6145a5bc436a33016a109c25165754b7b9ee12
rodrigoss88 <guidorodrigorivero@outlook.com>
2022-05-13 01:14:18 -0300

    jsriverorodrigo

--- js/saludoalert.js
+++ js/saludoalert.js
@@ -6,17 +6,18 @@ window.onload= function sal(){
   const inputOptions = new Promise((resolve) => {
     setTimeout(() => {
       resolve({
-        '<span class="red" >Perfecto que  te sientas bien,sigue asi vas por el buen camino! </span>': 'Bien',
-        'No te preocupes ,hoy puede mejorar todo,asi que estate tranquilo': 'Mas o menos',
-        'Todos tienen un mal dia , pero todo pasa y seguro lo hara en esta ocasion tambien': 'Mal'
+        '<span class="animo" >" Aquel que tiene un porqué para vivir se puede enfrentar a todos los «cómos»."<br>(Friedrich Nietzsche)</span>': 'Bien',
+        '<span class="animo" >" Los hombres olvidan siempre que la felicidad humana es una disposición de la mente y no una condición de las circunstancias."<br>(Locke)</span>': 'Mas o menos',
+        '<span class="animo" > " Puede que no podamos controlar lo que nos pasa, pero podemos controlar lo que ocurre en nuestro interior." <br>(Benjamin Franklin)</span>': 'Mal'
       })
     }, 1000)
   })
   
   const { value: color } = await Swal.fire({
-    title: 'Hola!',
+    title: '<b> Hola!</b>',
     html:'<b>Como te sientes el dia de hoy?</b>',
     input: 'radio',
+    confirmButtonText:'Listo',
     inputOptions: inputOptions,
     confirmButtonColor:'rgb(63,94,251)',
     inputValidator: (value) => {
@@ -29,13 +30,8 @@ window.onload= function sal(){
   if (color) {
     Swal.fire({ 
       title:`${color}`,
-      html:'<b>Aqui tienes muchas opciones para elegir </b> ',
-      confirmButtonText:'elegir',
-      padding:'',
-      footer:'',
-      width:'',
-      timer:'',
-      timerProgressBar:false,
+      html:' ',
+      confirmButtonText:'Seguir',
       toast:false,
       confirmButtonColor:'rgb(63,94,251)',
       showCloseButton:false, })
